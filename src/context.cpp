f8b2e020519d0a02a99911996fa8f77321d229a0
Gregory1234 <Gregory1234@users.noreply.github.com>
2018-03-25 17:40:44 +0200

    Fixed all errors

--- src/context.cpp
+++ src/context.cpp
@@ -1,1 +1,26 @@
 #include<context.h>
+
+GRL::IdentifierType GRL::CompilerContext::getIdentifierType(const std::string& s){
+        if(getFunction(s)!=nullptr)
+                return IdentifierType::FUNCTION;
+        else if(getClass(s)!=nullptr)
+                return IdentifierType::CLASS;
+        return IdentifierType::NOTHING;
+}
+GRL::Function* GRL::CompilerContext::getFunction(const std::string& s){
+        for(auto& f: functions){
+                if(f.name==s)
+                        return &f;
+        }
+        return nullptr;
+}
+GRL::Class* GRL::CompilerContext::getClass(const std::string& s){
+        for(auto& c: classes){
+                if(c.name==s)
+                        return &c;
+        }
+        return nullptr;
+}
+void GRL::CompilerContext::findGlobals(const std::string& fn){
+        //TODO
+}
